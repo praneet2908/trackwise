c4b97622b89c63e827500fb201b3ef1a8bc82590
praneet2908 <mallickpraneet2908@gmail.com>
2026-05-04 17:18:09 +0530

    Fix chdir for Render imports

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import sys
 import os
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
+
+os.chdir(os.path.join(os.path.dirname(__file__), 'backend'))
 
 from main import app
 
