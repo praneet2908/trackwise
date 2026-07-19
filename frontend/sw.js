b0266d435fbc94d2a74da5bd1cb78d6a066a6d12
praneet2908 <mallickpraneet2908@gmail.com>
2026-07-19 20:45:47 +0530

    fix: loadFromCloud mode bug + favicon + sw.js

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,8 +1,13 @@
-const CACHE = 'trackwise-v1';
+const CACHE = 'trackwise-v2';
 const FILES = ['/', '/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 });
 
 self.addEventListener('fetch', e => {
@@ -12,6 +17,8 @@ self.addEventListener('fetch', e => {
 self.addEventListener('push', e => {
   const data = e.data?.json() || {};
   self.registration.showNotification(data.title || 'TrackWise', {
-    body: data.body || 'Check your subscriptions!',
-      });
+    body: data.body || 'Check your spending!',
+    icon: '/icon-192.png',
+    badge: '/icon-192.png'
+  });
 });
